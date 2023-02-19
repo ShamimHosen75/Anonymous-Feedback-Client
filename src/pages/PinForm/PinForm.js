b30a1914fac7415829ee7fd495111f2a7c99be1c
ShamimHosen75 <developer.shamimhosen@gmail.com>
2023-02-20 00:45:15 +0600

    added PinForm Components

--- src/pages/PinForm/PinForm.js
+++ src/pages/PinForm/PinForm.js
@@ -1,50 +1,51 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-const PinForm = () => {
+function PinForm(props) {
   const [pin, setPin] = useState('');
   const [error, setError] = useState('');
 
+  useEffect(() => {
+    // code to show pop-up form on website visit
+  }, []);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (pin.length !== 4) {
-      setError('PIN must be exactly 4 digits');
-    } else {
-      // Do something with the PIN, like sending it to the backend for validation
-      console.log(pin);
-      setPin('');
-    }
+  
+    fetch('/api/check-pin', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ pin })
+    })
+      .then((response) => {
+        if (response.ok) {
+         // redirect to review page on success
+        } else {
+          setError('Invalid pin code');
+        }
+      })
+      .catch((error) => {
+        setError('Error occurred while checking pin code');
+      });
   };
 
   return (
     <div className="card-body py-5">
       <h4 className="text-3xl text-black text-center font-bold">Pin Code Form</h4>
-      <form onSubmit={handleSubmit}>
-      <label htmlFor="password" className="label">
-          <span className="label-text">Pin Code</span>
-      </label>
-      <input
-        type="text"
-        name="pin"
-        value={pin}
-        placeholder="Enter your pin"
-        onChange={(e) => setPin(e.target.value)}
-        min="0"
-        max="9999"
-        required
-        className="input input-bordered input-lg w-full max-w-xs"
-      />
-      {error && <p>{error}</p>}
-      <div className="form-control mt-5">
+       <form onSubmit={handleSubmit}>
+       <label>
+         Enter your 4 digit pin:
+        <input type="text" value={pin} onChange={(e) => setPin(e.target.value)} />
+       </label>
+       {error && <div className="error">{error}</div>}
+       <div className="form-control mt-5">
          <input
           type="submit"
           value="Submit"
           className="btn btn-primary text-white w-32  font-bold border-0"
          ></input>
         </div>
-    </form>
-  </div>
-    
+     </form>
+    </div>
   );
-};
-
+}
 export default PinForm;
