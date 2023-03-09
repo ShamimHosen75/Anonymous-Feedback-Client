8a2c550b44b53f910ae7632aedf1c4374191b17a
ShamimHosen75 <developer.shamimhosen@gmail.com>
2023-03-10 03:18:52 +0600

    code updated

--- src/pages/shared/NotFound/NotFound.js
+++ src/pages/shared/NotFound/NotFound.js
@@ -7,10 +7,10 @@ const NotFound = () => {
 
     return (
         <>
-            <div className='container'>
-                <div className='notFound-wrapper text-primary'>
-                  <h1 className='text-center text-4xl font-bold pt-9 pb-5'>Oops!</h1>
-                  <h4 className='text-center text-8xl font-bold'>404</h4>
+            <div>
+                <div>
+                  <h1 className='text-center text-black text-4xl font-bold pt-9 pb-4'>Oops!</h1>
+                  <h4 className='text-center text-black text-8xl font-bold'>404</h4>
                     <div className='flex justify-center items-center py-20'>
                     <button className='btn btn-primary  px-10 border-0 text-lg font-bold text-secondary hover:bg-black hover:text-white' onClick={() => navigate("/home")}>GO TO HOMEPAGE</button>
                     </div>
